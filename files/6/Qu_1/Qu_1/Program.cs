2bab1ff03491e4e144178443bbf5805ef26f1e78
Denismeru <mahtdaniil384@gmail.com>
2024-02-27 17:53:04 +0300

    Добавлена папка files

---
+++ files/6/Qu_1/Qu_1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+namespace Qu_1
+{
+    class Program
+    {
+        static void Main()
+        {
+            try
+            {
+                FileStream f = new FileStream("test.txt", FileMode.Create, FileAccess.ReadWrite);
+                byte[] x = new byte[10];
+                Random rd = new Random();
+                rd.NextBytes(x);
+                Console.Write("Массив х: ");
+                for (int i = 0;i<10;i++) Console.Write($"{x[i]} ");
+                Console.WriteLine();
+                f.Write(x, 0, 10);
+                int a;
+                bool b = false,c = false;
+                f.Seek(0, SeekOrigin.Begin);
+                for (int i = 0; i < 10; i++)
+                {
+                    a = f.ReadByte();
+                    int d = (int)Math.Sqrt(a);
+                    if (Math.Pow(d,2)==a)
+                    {
+                        b = true;
+                        if (!c)
+                        {
+                            Console.Write("Числа, которые являются квадратами некоторого целого числа: ");
+                            c = true;
+                        }
+                        Console.Write($"{a} ");
+                        
+                    }
+                }
+                if (!b) Console.Write("Нет чисел, которые являются квадратами некоторого целого числа.");
+                Console.WriteLine();
+                f.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Ошибка работы с файлом: " + e.Message);
+            }
+        }
+    }
+}
---
+++ files/7/Qu_2/Qu_2/Prog.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+namespace Qu_2
+{
+    class Prog
+    {
+        static void Main()
+        {
+            try
+            {
+                // назначение источника данных для ввода, вместо клавиатуры
+                StreamReader F = new StreamReader("f.txt");
+                Console.SetIn(F);
+                // назначение приемника данных, вместо экрана
+                StreamWriter G = new StreamWriter("g.txt");
+                Console.SetOut(G);
+
+                // тест перенаправленного ввода-вывода
+                string s = Console.ReadLine();
+                Console.WriteLine(s);
+                try
+                {
+                    throw new Exception("what is it?!");
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Что-то пошло не так...");
+                }
+                // закрытие потоков ввода-вывода
+                F.Close();
+                G.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error end: " + e.Message);
+                return;
+            }
+        }
+    }
+}
